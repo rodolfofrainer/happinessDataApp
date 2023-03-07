ccaed6f881e5e29842f164a54e3f54e4ca8c14b3
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-03-07 14:24:13 +0000

    add dynamically plotted graph from data frame

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('happy.csv')
+
+st.title('In Search of Happiness')
+
+x_axis = st.selectbox('Data for X-axis',
+                      ["GDP", "Happiness", "Generosity"])
+
+y_axis = st.selectbox('Data for Y-axis',
+                      ["GDP", "Happiness", "Generosity"])
+
+st.subheader(f'{x_axis} and {y_axis}')
+scatter_graph = px.scatter(
+    x=df[x_axis.lower()],
+    y=df[y_axis.lower()],
+    labels={"x": f"{x_axis}", "y": f"{y_axis}"})
+
+st.plotly_chart(scatter_graph)
